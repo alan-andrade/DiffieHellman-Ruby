80648223c6cf980caca55c3dc4921d4747247840
Alan Andrade <gallito7@gmail.com>
2011-04-13 20:34:22 -0500

    Se agrega el RC4 nativo de la aplicacion. Ya no se debe instalar la gem de 'ruby-rc4'

--- lib/dh.rb
+++ lib/dh.rb
@@ -1,3 +1,10 @@
-['key.rb', 'host.rb', 'primitive_root.rb', 'net.rb', 'handshake.rb', 'message.rb'].each do |file|
+[
+  'key.rb', 
+  'host.rb', 
+  'primitive_root.rb', 
+  'net.rb', 
+  'handshake.rb', 
+  'message.rb'
+].each do |file|
   require "./lib/dh/" + file
 end
